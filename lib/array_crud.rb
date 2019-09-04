a4c2c1cdffc383c2abea97fee7592c9e4ff0dfa6
Carrie Brunt <cdpb18@gmail.com>
2019-09-04 14:52:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   animals = ["cat", "dog", "fish", "hamster"]
-  
+  animals << "bird"
 end
 
 def add_element_to_start_of_array(array, element)
