6a109069e775a9953ae02fcdc9cc58b74c0cd9ec
Carrie Brunt <cdpb18@gmail.com>
2019-09-04 15:07:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
- famous_cats =  ["Cheshire Cat", "Puss in Boots", "Garfield"]
+ famous_cats =  ["Cheshire Cat", "am", "Garfield"]
  
 famous_cats[1]
 end
