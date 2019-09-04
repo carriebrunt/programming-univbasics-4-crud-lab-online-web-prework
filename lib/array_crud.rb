19fc22eb380958ec789196dbf735661196f2fcd8
Carrie Brunt <cdpb18@gmail.com>
2019-09-04 15:01:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  animals = ["cat", "dog", "fish", "hamster"]
+  animals = ["cat", "dog", "fish", "hamster", "arrays!"]
   arrays = animals.pop
   p animals
 end
